6bb555bd183fbe44181fedc5c0a67bbb39a5c602
charliejhadley <charliejhadley@gmail.com>
2022-06-02 21:41:24 +0100

    quarto rebuild!

--- static/data/data-processing_keeping-a-secret.R
+++
@@ -1,4 +0,0 @@
-library("tidyverse")
-library("here")
-library("secret")
-
--- themes/hugo-universal-theme/static/js/backported-popover.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function(){
-    $('[data-toggle="popover"]').popover(); 
-});
-
-
-$('[data-toggle="popover"]').popover({trigger: 'hover','placement': 'top'});
